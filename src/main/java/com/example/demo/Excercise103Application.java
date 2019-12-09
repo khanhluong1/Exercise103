2765df303a28d4679f9ced874cdc5e88454de758
khanhluong1 <derick.luong@gmail.com>
2019-12-09 16:17:15 -0500

    Exercise 3

---
+++ src/main/java/com/example/demo/Excercise103Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Excercise103Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Excercise103Application.class, args);
+    }
+
+}
